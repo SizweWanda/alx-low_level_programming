c47dac820bde998cce0c226bed8aad60eaeeae89
Sizwe Ntombela <sizwen18@gmail.com>
2022-06-08 11:53:36 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,9 @@
 /** main -prints putchar
  * Return: always 0
  */
-
-
 int main(void)
 {
-	_putchar('_')
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -18,5 +16,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
 }
