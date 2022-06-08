14b40b3493f8ad28d47c1807d6f84b501f2a7187
Sizwe Ntombela <sizwen18@gmail.com>
2022-06-08 13:14:12 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_10x -> prints alphabets 10 x
+ */
+
+void print_alphabet_10x(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		putchar('\n');
+	}
+
+
